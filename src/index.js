1fed5c04584976a383d325e1a10bab101ad5e7ff
MariiaSheinyk <mariasheynik@gmail.com>
2024-01-03 10:28:33 +0100

    Revert "poprtfolioProject"
    This reverts commit 8e9938f87988dcad0f27bb1547db51435fa1a332.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from "./components/router";
-import reportWebVitals from "./reportWebVitals";
-import App from "./App";
 import "./index.css";
-import SimpleMovingButton from "./components/Buttons/SimpleMovingButton/SimpleMovingButton";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const router = createBrowserRouter(routes);
-
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-      <SimpleMovingButton>
-          <App />
-      </SimpleMovingButton>
-    </RouterProvider>
+    <App />
   </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
